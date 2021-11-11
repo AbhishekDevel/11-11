e9c7b801f4259f0d1173d9e37684bcc49d89c713
Abhishekdevel <abhishek947122@gmail.com>
2021-11-11 20:53:17 +0530

    8:53-11-11

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ if (window.innerWidth >= 750){
   })
 } else{
   var typed = new Typed(".auto-input", {
-    strings: ["Anna Amelia", "Web Developer", "UI Designer"],
+    strings: ["Anna Amelia.", "Web Developer.", "UI Designer."],
     typeSpeed: 200,
     backSpeed: 200,
     loop: true
